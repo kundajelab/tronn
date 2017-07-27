6291a8140470de50dd27befd8bd8aefc7e6aad79
vervacity <dskim89@gmail.com>
2017-07-26 19:12:37 -0700

    clean up layers --> ops

--- tronn/util/tf_ops.py
+++ tronn/util/tf_ops.py
@@ -40,6 +40,32 @@ def maxnorm(norm_val=7):
     return None
 
 
+def threshold(input_tensor, th, val, name):
+    """Sets up a threshold layer
+    
+    Use for torch7 models (like deepSEA) that use
+    the threshold function instead of the ReLU function
+
+    Args:
+      input_tensor: input tensor
+      th: threshold value
+      val: the value to output if below threshold
+
+    Returns:
+      out_tensor: tensor after threshold
+    """
+    with tf.variable_scope(name) as scope:
+
+        # First figure out where values are less than
+        lessthan_tensor = tf.cast(tf.lesser(input_tensor, th), tf.int32)
+        greaterthan_tensor = tf.cast(tf.greater(input_tensor, th), tf.int32)
+
+        # Then make thresholded values equal to val and the rest are the same
+        out_tensor = input_tensor * greaterthan_tensor + val * lessthan_tensor
+
+    return out_tensor
+    
+
 def order_preserving_k_max(input_tensor, k):
     """Finds values k largest entries for the last dimension and 
     returns them in the order they originally appeared.
