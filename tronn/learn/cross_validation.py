5900ed00b08e6ad2ca74a865aadec70111635c3b
vervacity <dskim89@gmail.com>
2017-11-01 16:42:22 -0700

    5-fold splits for CV

--- tronn/learn/cross_validation.py
+++ tronn/learn/cross_validation.py
@@ -2,26 +2,26 @@
 # consistent cross validation across functions
 
 
-
 def setup_cv(data_files, cvfold=0):
     """Helper function to choose good CV folds
     Just hard-coded 3 cv folds
     """
     # never train with chrY
-    data_files.remove("chrY")
-    data_files = sorted(data_files)
+    data_files = sorted([data_file for data_file in data_files if "chrY" not in data_file])
 
-    # 1 for validation, 2 for test, never chrX or Y (or tiny ones)
-    cv_start_indices = [1, 7, 16]
+    # 1 for validation, 2 for test, never chrX
+    cv_start_indices = [1, 7, 16, 4, 19]
     cv_start_idx = cv_start_indices[cvfold]
-    
+
+    # set up splits
     train_files = list(data_files)
     del train_files[cv_start_idx:cv_start_idx+3]
     valid_files = [data_files[cv_start_idx]]
     test_files = data_files[cv_start_idx+1:cv_start_idx+3]
-    
-    print "Train:", train_files
-    print "Validation:", valid_files
-    print "Test:", test_files
 
+    # logging
+    print "Train:", [filename.split(".")[-2] for filename in train_files]
+    print "Validation:", [filename.split(".")[-2] for filename in valid_files]
+    print "Test:", [filename.split(".")[-2] for filename in test_files]
+    
     return train_files, valid_files, test_files
