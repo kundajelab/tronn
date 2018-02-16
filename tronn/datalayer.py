bug fixes to integrated gradients

tronn/datalayer.py
@@ -354,7 +354,7 @@ def filter_through_labels(features, labels, metadata, filter_tasks, batch_size):
         [features_filtered, labels_filtered, metadata_filtered],
         batch_size,
         capacity=100000,
-        num_threads=1, # adjust as needed
+        num_threads=4, # adjust as needed
         enqueue_many=True,
         name="filter_batcher")
     
@@ -459,7 +459,7 @@ def load_step_scaled_data_from_filename_list(
     features = [tf.expand_dims(tensor, axis=0) for tensor in tf.unstack(features, axis=0)]
     labels = [tf.expand_dims(tensor, axis=0) for tensor in tf.unstack(labels, axis=0)]
     metadata = [tf.expand_dims(tensor, axis=0) for tensor in tf.unstack(metadata, axis=0)]
-    
+
     join_list = []
     new_features = []
     new_labels = []
@@ -474,8 +474,9 @@ def load_step_scaled_data_from_filename_list(
         scaled_metadata = tf.concat(
             [metadata[example_idx]
              for i in xrange(1, steps+1)], axis=0)
-        join_list.append((scaled_features, scaled_labels, scaled_metadata))
 
+        join_list.append((scaled_features, scaled_labels, scaled_metadata))
+        
     # try batch join
     features, labels, metadata = tf.train.batch_join(
         join_list,

tronn/interpretation/interpret.py
@@ -187,7 +187,7 @@ def interpret(
 
                     h5_handler.store_example(region_arrays)
                     total_examples += 1
-                    
+
                     # conditions for visualization: logits > 0,
                     # and mean(importances) > 0, AND not empty net
                     # set up real condition
@@ -219,9 +219,9 @@ def interpret(
                     if (sample_size is not None) and (total_examples >= sample_size):
                         break
 
-
-
-            except tf.errors.OutOfRangeError:
+            #except tf.errors.OutOfRangeError:
+            except Exception as e:
+                print e
                 print "Done reading data"
                 # add in last of the examples
 

tronn/nets/filter_nets.py
@@ -22,8 +22,9 @@ def rebatch(features, labels, config, is_training=False):
     outputs = tf.train.batch(
         tensors,
         batch_size,
-        capacity=batch_size*5 + 100,
-        num_threads=1,
+        capacity=100000,
+        #capacity=batch_size*10 + 100,
+        num_threads=4,
         enqueue_many=True,
         name="rebatch_queue")
 
@@ -76,7 +77,7 @@ def filter_through_mask(
             batch_size,
             #capacity=128+5, # keep capacity small to move faster?
             capacity=batch_size*10 + 100,
-            num_threads=2, # 2
+            num_threads=4, # 2
             enqueue_many=True,
             name="filtering_queue")
     else:
@@ -106,7 +107,7 @@ def filter_by_accuracy(features, labels, config, is_training=False):
     """Filter by accuracy
     """
     # get params needed
-    probs = config.get("filter_probs")
+    probs = config["outputs"].get("probs")
     task_indices = config.get("importance_task_indices")
     batch_size = config.get("batch_size")
     acc_threshold = config.get("acc_threshold", 0.8)
@@ -140,7 +141,7 @@ def filter_by_accuracy(features, labels, config, is_training=False):
     # filter
     with tf.variable_scope("accuracy_filter"):
         features, labels, config = filter_through_mask(
-            features, labels, config, condition_mask, use_queue=False)
+            features, labels, config, condition_mask, use_queue=True)
 
     return features, labels, config
 

tronn/nets/importance_nets.py
@@ -47,11 +47,11 @@ def integrated_gradients(features, labels, config, is_training=False):
     # for features, reduce mean
     features = tf.reduce_mean(features, axis=0, keep_dims=True)
 
-    # for everything else, just grab the first of the batch (they should all be the same)
-    labels = tf.expand_dims(tf.unstack(labels, axis=0)[0], axis=0)
+    # for everything else, just grab the LAST of the batch (which is the non-scaled sequence)
+    labels = tf.expand_dims(tf.unstack(labels, axis=0)[-1], axis=0)
     for key in config["outputs"].keys():
         config["outputs"][key] = tf.expand_dims(
-            tf.unstack(config["outputs"][key], axis=0)[0], axis=0)
+            tf.unstack(config["outputs"][key], axis=0)[-1], axis=0)
 
     return features, labels, config
 
@@ -64,7 +64,7 @@ def multitask_importances(features, labels, config, is_training=False):
     assert is_training == False
 
     # get configs
-    anchors = config.get("anchors")
+    anchors = config["outputs"].get("logits")
     task_indices = config.get("importance_task_indices")
     backprop = config.get("backprop", "input_x_grad")
 
@@ -96,10 +96,10 @@ def multitask_importances(features, labels, config, is_training=False):
 
     # adjust labels and configs as needed here
     if backprop == "integrated_gradients":
-        labels = tf.expand_dims(tf.unstack(labels, axis=0)[0], axis=0)
+        labels = tf.expand_dims(tf.unstack(labels, axis=0)[-1], axis=0)
         for key in config["outputs"].keys():
             config["outputs"][key] = tf.expand_dims(
-                tf.unstack(config["outputs"][key], axis=0)[0], axis=0)
+                tf.unstack(config["outputs"][key], axis=0)[-1], axis=0)
 
         features, labels, config = rebatch(features, labels, config)
 