bug fixes integrated gradients

tronn/datalayer.py
@@ -468,29 +468,35 @@ def load_step_scaled_data_from_filename_list(
         scaled_features = tf.concat(
             [(float(i)/steps) * features[example_idx]
              for i in xrange(1, steps+1)], axis=0)
+        new_features.append(scaled_features)
+        
         scaled_labels = tf.concat(
             [labels[example_idx]
              for i in xrange(1, steps+1)], axis=0)
+        new_labels.append(scaled_labels)
+        
         scaled_metadata = tf.concat(
             [metadata[example_idx]
              for i in xrange(1, steps+1)], axis=0)
+        new_metadata.append(scaled_metadata)
 
-        join_list.append((scaled_features, scaled_labels, scaled_metadata))
+        #join_list.append((scaled_features, scaled_labels, scaled_metadata))
         
     # try batch join
-    features, labels, metadata = tf.train.batch_join(
-        join_list,
-        batch_size,
-        capacity=100000,
-        enqueue_many=True,
-        name="scaled_data_batcher")
-        
     if False:
+        features, labels, metadata = tf.train.batch_join(
+            join_list,
+            batch_size,
+            capacity=100000,
+            enqueue_many=True,
+            name="scaled_data_batcher")
+        
+    if True:
         # concatenate all
         features = tf.concat(new_features, axis=0)
         labels = tf.concat(new_labels, axis=0)
         metadata = tf.concat(new_metadata, axis=0)
-
+        
         # put these into an ordered queue    
         features, labels, metadata = tf.train.batch(
             [features, labels, metadata],

tronn/nets/filter_nets.py
@@ -47,6 +47,7 @@ def filter_through_mask(
         labels,
         config,
         condition_mask,
+        num_threads=1,
         use_queue=True):
     """Given a precalculated condition mask, filter variables
     """
@@ -76,8 +77,9 @@ def filter_through_mask(
             tensors,
             batch_size,
             #capacity=128+5, # keep capacity small to move faster?
-            capacity=batch_size*10 + 100,
-            num_threads=4, # 2
+            capacity=100000,
+            #capacity=batch_size*10 + 100,
+            num_threads=num_threads,
             enqueue_many=True,
             name="filtering_queue")
     else:
@@ -131,8 +133,8 @@ def filter_by_accuracy(features, labels, config, is_training=False):
 
     # calculate accuracy
     correct_predictions = tf.logical_not(tf.logical_xor(
-        tf.cast(labels, tf.bool),
-        tf.greater_equal(probs, 0.5))) # {N, tasks}
+        tf.cast(filter_labels, tf.bool),
+        tf.greater_equal(filter_probs, 0.5))) # {N, tasks}
     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32), axis=1) # {N}
     
     # set condition
@@ -141,7 +143,7 @@ def filter_by_accuracy(features, labels, config, is_training=False):
     # filter
     with tf.variable_scope("accuracy_filter"):
         features, labels, config = filter_through_mask(
-            features, labels, config, condition_mask, use_queue=True)
+            features, labels, config, condition_mask, use_queue=True, num_threads=1)
 
     return features, labels, config
 
@@ -177,7 +179,7 @@ def filter_by_importance(features, labels, config, is_training=False):
     # filter
     with tf.variable_scope("importance_filter"):
         features, labels, config = filter_through_mask(
-            features, labels, config, condition_mask)
+            features, labels, config, condition_mask, num_threads=1)
     
     return features, labels, config
 

tronn/nets/importance_nets.py
@@ -47,6 +47,7 @@ def integrated_gradients(features, labels, config, is_training=False):
     # for features, reduce mean
     features = tf.reduce_mean(features, axis=0, keep_dims=True)
 
+    # NOTE: DONT adjust here, it references the original dict
     # for everything else, just grab the LAST of the batch (which is the non-scaled sequence)
     labels = tf.expand_dims(tf.unstack(labels, axis=0)[-1], axis=0)
     for key in config["outputs"].keys():
@@ -82,7 +83,8 @@ def multitask_importances(features, labels, config, is_training=False):
     print importances_fn
     
     # split out anchors by task
-    anchors = [tf.expand_dims(tensor, axis=1) for tensor in tf.unstack(anchors, axis=1)] # {N, 1, pos, C}
+    anchors = [tf.expand_dims(tensor, axis=1)
+               for tensor in tf.unstack(anchors, axis=1)] # {N, 1}
 
     # get task specific importances
     task_importances = []
@@ -93,7 +95,8 @@ def multitask_importances(features, labels, config, is_training=False):
         task_importances.append(task_importance)
 
     features = tf.concat(task_importances, axis=1) # {N, task, pos, C}
-
+    
+    # tODO check the config here?
     # adjust labels and configs as needed here
     if backprop == "integrated_gradients":
         labels = tf.expand_dims(tf.unstack(labels, axis=0)[-1], axis=0)
@@ -101,8 +104,14 @@ def multitask_importances(features, labels, config, is_training=False):
             config["outputs"][key] = tf.expand_dims(
                 tf.unstack(config["outputs"][key], axis=0)[-1], axis=0)
 
-        features, labels, config = rebatch(features, labels, config)
+        # NOTE: rebatch might be the issue?
+        #features, labels, config = rebatch(features, labels, config)
 
+        #print features
+        #print labels
+        #print config["outputs"]
+        #quit()
+        
     return features, labels, config
 
 