8faffde675cd5ed5feb2cd85579b1e23b0b7a8fb
vervacity <dskim89@gmail.com>
2018-03-30 19:11:53 -0700

    updates to plotting

--- R/plot.pwm_x_pwm.mut.from_h5.R
+++ R/plot.pwm_x_pwm.mut.from_h5.R
@@ -12,6 +12,8 @@ args <- commandArgs(trailingOnly=TRUE)
 h5_file <- args[1]
 dataset_key <- args[2]
 
+mut_names <- c("SMAD3", "TFAP2B")
+
 #h5ls(h5_file)
 
 se <- function(x) sd(x) / sqrt(length(x))
@@ -19,6 +21,35 @@ se <- function(x) sd(x) / sqrt(length(x))
 # read in data
 data <- h5read(h5_file, dataset_key, read.attributes=TRUE)#[,2,]
 pwm_data <- h5read(h5_file, "pwm-scores.taskidx-0", read.attributes=TRUE)
+logits_data <- h5read(h5_file, "delta_logits", read.attributes=TRUE)[,1,]
+
+print(dim(logits_data))
+
+# organize the logits, add fake data in
+logits_means <- apply(logits_data, 1, mean)
+logits_means <- t(data.frame(
+    logits=logits_means,
+    row.names=mut_names))
+
+rownames(logits_means) <- "a"
+logits_means <- rbind(logits_means, rep(-1, ncol(logits_means)))
+rownames(logits_means)[nrow(logits_means)] <- "b"
+logits_means <- rbind(logits_means, rep(1, ncol(logits_means)))
+rownames(logits_means)[nrow(logits_means)] <- "c"
+
+#print(logits_means)
+#quit()
+
+logits_se <- apply(logits_data, 1, se)
+logits_se <- t(data.frame(
+    logits=logits_se,
+    row.names=mut_names))
+
+rownames(logits_se) <- "a"
+logits_se <- rbind(logits_se, rep(-1, ncol(logits_se)))
+rownames(logits_se)[nrow(logits_se)] <- "b"
+logits_se <- rbind(logits_se, rep(1, ncol(logits_se)))
+rownames(logits_se)[nrow(logits_se)] <- "c"
 
 # organize the pwm scores
 print(dim(pwm_data))
@@ -32,75 +63,53 @@ pwm_scores_se <- t(data.frame(
     motifs=pwm_scores_se,
     row.names=attr(pwm_data, "pwm_names")))
 
-
-# transpose (fastest changing axis is opposite order in R vs python)
+# organize the mutational response data
 data <- aperm(data, c(3, 2, 1))
 
-# mean across example axis
 data_mut_means <- apply(data, c(2, 3), mean)
 colnames(data_mut_means) <- attr(pwm_data, "pwm_names")
 rownames(data_mut_means) <- c("SMAD3", "TFAP2B")
 
-
-# standard error
 data_mut_sd <- apply(data, c(2, 3), se)
 colnames(data_mut_sd) <- attr(pwm_data, "pwm_names")
 rownames(data_mut_sd) <- c("SMAD3", "TFAP2B")
 
+# ===============
+
+
 # order by strongest effect
+ordering_vals <- order(colSums(data_mut_means))
+#data_mut_means <- rbind(data_mut_means, ordering_vals)
+#rownames(data_mut_means)[nrow(data_mut_means)] <- "order"
+
+
+map <- setNames(
+    1:ncol(data_mut_means)+10,
+    colnames(data_mut_means)[ordering_vals])
+
+
 data_mut_means <- data_mut_means[,order(colSums(data_mut_means))]
+print(colnames(data_mut_means))
+
+print(ordering_vals)
 
 
-# color palette
-#my_palette <- colorRampPalette(brewer.pal(9, "Blues"))(49)
-my_palette <- rev(colorRampPalette(brewer.pal(9, "RdBu"))(49))
 
-# grid
-mylmat = rbind(c(0,3,0),c(2,1,0),c(0,4,0))
-mylwid = c(2,10,2)
-mylhei = c(0.5,2,0.5)
-
-# heatmap
-heatmap_file <- paste(
-    paste(
-        sub(".h5", "", h5_file),
-        dataset_key,
-        "1", sep="-"), # to match 0-START from python
-    "pdf", sep=".")
-heatmap_file <- "testing.pdf"
-pdf(heatmap_file, height=18, width=20)
-heatmap.2(
-    as.matrix(data_mut_means),
-    Rowv=FALSE,
-    Colv=FALSE,
-    #dendrogram="column",
-    dendrogram="none",
-    trace="none",
-    density.info="none",
-    #labRow="",
-    cexCol=0.5,
-    keysize=0.1,
-    key.title=NA,
-    key.xlab=NA,
-    key.par=list(pin=c(4,0.1),
-        mar=c(9.1,0,2.1,0),
-        mgp=c(3,2,0),
-        cex.axis=2.0,
-        font.axis=2),
-    margins=c(3,0),
-    lmat=mylmat,
-    lwid=mylwid,
-    lhei=mylhei,
-    col=my_palette)
-dev.off()
+# ==================
 
 # ggplot2 version
 library(ggplot2)
 library(reshape)
 
 data_melted <- melt(data_mut_means)
+print(head(data_melted))
+
 colnames(data_melted) <- c("target", "response", "delta")
 data_melted$id <- paste(data_melted$target, data_melted$response, sep="_to_")
+
+# set up correct x
+data_melted$x <- map[as.character(data_melted$response)]
+
 
 # melt the sd also and merge in
 sd_melted <- melt(data_mut_sd)
@@ -120,30 +129,75 @@ pwm_means_melted$response <- NULL
 pwm_se_melted <- melt(pwm_scores_se)
 colnames(pwm_se_melted) <- c("target", "response", "sd")
 pwm_se_melted$id <- paste(pwm_se_melted$target, pwm_se_melted$response, sep="_to_")
-#pwm_means_melted$target <- NULL
-#pwm_means_melted$response <- NULL
 
 pwm_full_melted <- merge(pwm_means_melted, pwm_se_melted, by="id")
+pwm_full_melted$x <- map[as.character(pwm_full_melted$response)]
 
 # and rbind all together
 data_melted <- rbind(data_melted, pwm_full_melted)
-#data_melted <- merge(data_melted, pwm_full_melted, by="id")
 
-print(head(pwm_full_melted))
-print(dim(pwm_full_melted))
+# melt the logits and merge in
+logit_means_melted <- melt(logits_means)
+colnames(logit_means_melted) <- c("response", "target", "delta")
+logit_means_melted$id <- paste(logit_means_melted$target, logit_means_melted$response, sep="_to_")
+logit_means_melted$target <- NULL
+logit_means_melted$response <- NULL
+
+logit_se_melted <- melt(logits_se)
+colnames(logit_se_melted) <- c("response", "target", "sd")
+logit_se_melted$id <- paste(logit_se_melted$target, logit_se_melted$response, sep="_to_")
+
+logit_full_melted <- merge(logit_means_melted, logit_se_melted, by="id")
+#logit_full_melted$response <- factor(
+#    logit_full_melted$response, levels=c(1, 2, 3), ordered=TRUE)
+
+print(logit_full_melted$response)
+
+#logit_full_melted$response <- factor(
+#    logit_full_melted$response, levels=rev(rownames(logits_means)), ordered=TRUE)
+
+#print(logit_full_melted$response)
+
+logit_full_melted$x <- logit_full_melted$delta
+
+
+#logit_full_melted$tmp <- logit_full_melted$response
+#logit_full_melted$response <- logit_full_melted$delta
+#logit_full_melted$delta <- logit_full_melted$tmp
+#logit_full_melted$delta <- rep(0, nrow(logit_full_melted))
+#logit_full_melted$tmp <- NULL
+
+
+# add in extra column as factor
+data_melted$horiz_factor <- rep("motifs", nrow(data_melted))
+logit_full_melted$horiz_factor <- rep("logits", nrow(logit_full_melted))
+
+# adjust response column
+#data_melted$response <- as.character(data_melted$response)
+
+
+# and rbind all together
+data_melted <- rbind(data_melted, logit_full_melted)
+print(tail(data_melted, n=10L))
+
 
 # fix order of respondents
+
+#data_melted$response[data_melted$horiz_factor == "logits"] <- "0.0"
+
 data_melted$response <- factor(
     data_melted$response,
-    levels=colnames(data_mut_means))
+    levels=c(rev(rownames(logits_means)), colnames(data_mut_means)),
+    ordered=TRUE)
+    #labels=c("0", "-1", colnames(data_mut_means)))
+
+print(head(data_melted$response))
 
 data_melted$target <- factor(
-    data_melted$targe,
+    data_melted$target,
     levels=c("motifs", "TFAP2B", "SMAD3"))
 
-data_melted$delta_2 <- data_melted$delta
-print(head(data_melted))
-
+print(tail(data_melted))
 
 #data_melted$target <- as.numeric(data_melted$target)
 
@@ -157,153 +211,80 @@ print(head(data_melted))
         
 #ggsave("testing2.pdf", height=4, width=16)
 
+#data_melted$x <- nrow(data_melted):1
 
-p <- ggplot(data_melted, aes(x=response, y=delta)) +
-    facet_grid(target ~ ., scales="free") +
-        geom_col(data=subset(data_melted, target=="motifs"), aes(y=delta, fill=-delta)) +
+print(tail(data_melted))
+
+p <- ggplot(data_melted, aes(x=x, y=delta)) +
+    facet_grid(target ~ horiz_factor, scales="free", space="free_x", switch="y") +  # space=free_x
+
+        geom_col(
+            data=subset(data_melted, target=="motifs"),
+            aes(y=delta, fill=-delta)) +
 
 
-            geom_errorbar(aes(ymin=delta-2*sd, ymax=delta+2*sd, color=delta), width=0.05) +
-        geom_point(data=subset(data_melted, target=="TFAP2B"), aes(y=delta, color=delta, size=-delta)) +
-            geom_point(data=subset(data_melted, target=="SMAD3"), aes(y=delta, color=delta, size=-delta)) +
+            geom_errorbar(
+                data=subset(data_melted, horiz_factor=="motifs"),
+                aes(ymin=delta-2*sd, ymax=delta+2*sd, color=delta), width=0.05) +
+            geom_point(
+                data=subset(data_melted, horiz_factor=="motifs" & target=="TFAP2B"),
+                aes(y=delta, color=delta, size=-delta)) +
+            geom_point(
+                data=subset(data_melted, horiz_factor=="motifs" & target=="SMAD3"),
+                aes(y=delta, color=delta, size=-delta)) +
 
+            # this gets the spread out on the left
+            geom_point(
+                data=subset(data_melted, horiz_factor=="logits"),
+                alpha=0.00,
+                aes(x=-5, y=0, color=0)) + #x=10*delta, y=0
 
+            geom_point(
+                data=subset(data_melted, horiz_factor=="logits"),
+                alpha=0.00,
+                aes(x=5, y=0, color=0)) + #x=10*delta, y=0
 
+            # this gets the labels on
+            #geom_point(
+             #   data=subset(data_melted, horiz_factor=="logits"),
+             #   alpha=0.00,
+             #   aes(y=0, color=0)) + #x=10*delta, y=0
+                
+            geom_point(
+                data=subset(data_melted, response=="a" & horiz_factor=="logits"),
+                aes(x=5*delta, y=0)) + #x=10*delta, y=0
+
+            geom_segment(
+                data=subset(data_melted, response=="a" & horiz_factor=="logits"),
+                aes(x=0, y=0, xend=5*delta, yend=0)) + #x=10*delta, y=0
+
+            #geom_segment(
+            #    data=subset(data_melted, response=="a" & horiz_factor=="logits"),
+            #    aes(x=0, y=0, xend=0, yend=0)) + #x=10*delta, y=0
+
+                #expand_limits(x=-1) +
+    #scale_x_discrete(breaks=1:200) +
+
+                scale_y_continuous(position="right") +
     scale_fill_gradient(high="white", low="steelblue") +
             #geom_bar(data=subset(data_melted, target=="motif"), stat="identity", aes(color=target)) +
         theme_bw() +
+                              #geom_vline(xintercept=0, color="gray") +
+                scale_x_continuous(
+                    breaks=c(seq(-5, 5, length.out=9), 1:ncol(data_mut_means)+10),
+                    labels=c(seq(-1, 1, length.out=9), colnames(data_mut_means)),
+                    expand=c(0,0)) +
+
+            
             theme(
-                panel.grid.major=element_blank(),
-                axis.text.x=element_text(size=5, angle=60, hjust=1))
+                panel.grid.major.x=element_blank(),
+                panel.grid.minor=element_blank(),
+                axis.text.x=element_text(size=5, angle=60, hjust=1),
+                axis.text.y=element_text(size=5),
+                legend.text=element_text(size=5),
+                legend.key.size=unit(0.5, "line"),
+                legend.spacing=unit(0.5, "line"),
+                strip.background=element_blank())
         
 ggsave("testing2.pdf", height=4, width=16)
 
-
-
-quit()
-
-
-
-
-
-# asdfasdfasdf
-
-
-
-
-
-pwm_data <- t(pwm_data)
-print(dim(data))
-print(dim(pwm_data))
-
-
-quit()
-
-# normalize
-rowmax <- apply(pwm_data, 1, function(x) max(x))
-pwm_norm <- pwm_data / rowmax
-pwm_norm[is.na(pwm_norm)] <- 0
-pwm_data <- pwm_norm
-
-data_norm <- data
-
-# get mean and standard dev
-data_stds <- apply(data_norm, 2, sd)
-data_means <- apply(data_norm, 2, mean)
-
-print(data_means)
-print(sort(data_means))
-
-quit()
-
-# draw ordered sample
-plottable_nrow <- 1500
-if (nrow(data_norm) > plottable_nrow) {
-    skip_int <- as.integer(nrow(data_norm) / plottable_nrow)
-    ordered_sample_indices <- seq(1, nrow(data_norm), skip_int)
-    data_ordered <- data_norm[ordered_sample_indices,]
-    pwm_data <- pwm_data[ordered_sample_indices,]
-} else {
-    data_ordered <- data_norm
-}
-
-# order out here
-hc <- hclust(dist(data_ordered))
-data_ordered <- data_ordered[hc$order,]
-pwm_data <- pwm_data[hc$order,]
-
-# color palette
-my_palette <- colorRampPalette(brewer.pal(9, "Reds"))(49)
-my_palette <- rev(colorRampPalette(brewer.pal(9, "RdBu"))(49))
-
-# grid
-mylmat = rbind(c(0,3,0),c(2,1,0),c(0,4,0))
-mylwid = c(2,6,2)
-mylhei = c(0.5,12,1.5)
-
-# heatmap
-heatmap_file <- paste(
-    paste(
-        sub(".h5", "", h5_file),
-        dataset_key,
-        "1", sep="-"), # to match 0-START from python
-    "pdf", sep=".")
-pdf(heatmap_file, height=18, width=20)
-heatmap.2(
-    as.matrix(data_ordered),
-    Rowv=FALSE,
-    Colv=TRUE,
-    dendrogram="column",
-    trace="none",
-    density.info="none",
-    labRow="",
-    cexCol=0.5,
-    keysize=0.1,
-    key.title=NA,
-    key.xlab=NA,
-    key.par=list(pin=c(4,0.1),
-        mar=c(9.1,0,2.1,0),
-        mgp=c(3,2,0),
-        cex.axis=2.0,
-        font.axis=2),
-    margins=c(3,0),
-    lmat=mylmat,
-    lwid=mylwid,
-    lhei=mylhei,
-    col=my_palette)
-dev.off()
-
-
-# heatmap
-my_palette <- colorRampPalette(brewer.pal(9, "Reds"))(49)
-heatmap_file <- paste(
-    paste(
-        sub(".h5", "", h5_file),
-        dataset_key,
-        "0", sep="-"), # to match 0-START from python
-    "example_x_pwm",
-    "pdf", sep=".")
-pdf(heatmap_file, height=18, width=20)
-heatmap.2(
-    as.matrix(pwm_data),
-    Rowv=FALSE,
-    Colv=TRUE,
-    dendrogram="column",
-    trace="none",
-    density.info="none",
-    labRow="",
-    cexCol=0.5,
-    keysize=0.1,
-    key.title=NA,
-    key.xlab=NA,
-    key.par=list(pin=c(4,0.1),
-        mar=c(9.1,0,2.1,0),
-        mgp=c(3,2,0),
-        cex.axis=2.0,
-        font.axis=2),
-    margins=c(3,0),
-    lmat=mylmat,
-    lwid=mylwid,
-    lhei=mylhei,
-    col=my_palette)
-dev.off()
