4530194cd2a251c25b47eefec82d20733cc15664
vervacity <dskim89@gmail.com>
2018-10-15 15:33:35 -0700

    tensorflow contrib folder

---
+++ tronn/contrib/tensorflow/nets.py
@@ -0,0 +1,29 @@
+"""description: user defined networks
+"""
+
+"""
+To add a network:
+
+please note that it must be a function that takes two args:
+
+def nn(inputs, params):
+    # stuff happens here
+
+    return outputs, params
+
+inputs and outputs are dictionaries of tensors.
+params is a dictionary of options.
+
+once defined, add to net_fns to be accessible on 
+the command line to other functions
+
+"""
+
+
+
+
+
+
+net_fns = {
+
+}
