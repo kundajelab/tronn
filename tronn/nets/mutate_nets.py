03b3834f7fa92505ea4aff7112a8f851a919c5f7
vervacity <dskim89@gmail.com>
2018-11-08 13:55:52 -0800

    updates to mutate fns, move gettting max position to motif nets

--- tronn/nets/mutate_nets.py
+++ tronn/nets/mutate_nets.py
@@ -21,36 +21,28 @@ class Mutagenizer(object):
 
         
     def preprocess(self, inputs, params):
-        # determine where to mutagenize
-        # base level - search for motif
-        # requires motif score thresh map {N, task, pos, M} and filter_width
-        # base level, returns the pos of greatest M (top k)
-        assert inputs.get(DataKeys.WEIGHTED_SEQ_PWM_SCORES_THRESH) is not None
-        assert params.get("manifold") is not None # {M}
+        """extract the positions for only the desired motifs (sig pwms)
+        """
+        assert inputs.get(DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX) is not None
+        assert inputs.get(DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_VAL) is not None
+        assert params.get("sig_pwms") is not None
+
+        # collect features, TODO move others to old?
+        indices = tf.cast(inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX], tf.int64)
+        vals = inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_VAL]
         outputs = dict(inputs)
         
-        # TODO should this be a separate function in motifs?
-        # features - look at global top score positions
-        features = inputs[DataKeys.WEIGHTED_SEQ_PWM_SCORES_THRESH] # {N, task, pos, M}
-        features = tf.reduce_max(features, axis=1) # {N, pos, M}
-        features = tf.transpose(features, perm=[0,2,1]) # {N, M, pos}
+        # gather sig pwm max positions and vals
+        sig_pwms_indices = np.where(params["sig_pwms"])[0] # {mutM}
+        params["num_aux_examples"] = sig_pwms_indices.shape[0]
+        indices = tf.gather(indices, sig_pwms_indices, axis=1)
+        vals = tf.gather(vals, sig_pwms_indices, axis=1)
+
+        #print indices
+        #print vals
+        #quit()
         
-        # params
-        num_mutations = params.get("num_mutations", 1)
-        
-        # get the max positions ON THE ACTIVE REGION
-        # NOTE that these are indices for the short region!!
-        vals, indices = tf.nn.top_k(features, k=num_mutations, sorted=True)
-        
-        # and then only keep the ones that filter through pwm vector
-        with h5py.File(params["manifold"], "r") as hf:
-            pwm_mask = hf[DataKeys.MANIFOLD_PWM_SIG_CLUST_ALL][:]
-        params["num_aux_examples"] = np.sum(pwm_mask > 0)
-        sig_pwm_indices = np.where(pwm_mask > 0)[0]
-        vals = tf.gather(vals, sig_pwm_indices, axis=1)
-        indices = tf.gather(indices, sig_pwm_indices, axis=1)
-        
-        # save
+        # save adjusted
         outputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_VAL] = vals # {N, mut_M, k}
         outputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX] = indices # {N, mut_M, k}
         outputs[DataKeys.MUT_MOTIF_PRESENT] = tf.reduce_any(tf.greater(vals, 0), axis=2) # {N, mut_M}
@@ -62,7 +54,7 @@ class Mutagenizer(object):
             grad_mins = tf.argmin(grad_mins, axis=3) # {N, 1, 1000}
             grad_mins = tf.one_hot(grad_mins, 4) # {N, 1, 1000, 4}
             outputs["grad_mins"] = grad_mins
-        
+
         return outputs, params
 
     
@@ -149,9 +141,10 @@ class Mutagenizer(object):
         # for each motif, run map fn to mutagenize
         features = inputs[DataKeys.ORIG_SEQ] # {N, 1, 1000, 4}
         orig_seq_len = features.get_shape().as_list()[2]
-        LEFT_CLIP = 420
-        position_indices = tf.add(inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX], LEFT_CLIP) # {N, mut_M, k}
-        inputs["test.position_indices"] = position_indices
+        position_indices = inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX]
+        #LEFT_CLIP = 420
+        #position_indices = tf.add(inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX], LEFT_CLIP) # {N, mut_M, k}
+        #inputs["test.position_indices"] = position_indices
         motif_present = inputs[DataKeys.MUT_MOTIF_PRESENT] # {N, mut_M}
         
         # choose what kind of mutagenesis
