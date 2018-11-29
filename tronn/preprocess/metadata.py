7e0b06cc04069230f3f369e3e07b148cf698453e
vervacity <dskim89@gmail.com>
2018-11-28 16:59:00 -0800

    clean up metadata saving

--- tronn/preprocess/metadata.py
+++ tronn/preprocess/metadata.py
@@ -4,13 +4,14 @@ import h5py
 
 import numpy as np
 
+from tronn.util.utils import DataKeys
 
-def save_metadata(bed_file, h5_file, key="example_metadata"):
+
+def save_metadata(bed_file, h5_file, key=DataKeys.SEQ_METADATA):
     """save out metadata into h5 file
     """
     # read in metadata as numpy
     bed_array = np.loadtxt(bed_file, delimiter="\t", dtype="S1000")
-    print bed_array.shape
     
     # save into h5 file
     with h5py.File(h5_file, "a") as hf:
