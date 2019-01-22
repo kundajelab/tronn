98c6d09eb48020accf084e68ebe32eeac940e3a6
vervacity <dskim89@gmail.com>
2019-01-22 13:32:22 -0800

    updates to build grammars

--- tronn/buildgrammars_cmd.py
+++ tronn/buildgrammars_cmd.py
@@ -3,6 +3,8 @@
 import h5py
 import logging
 
+import numpy as np
+import networkx as nx
 import seaborn as sns
 
 from tronn.interpretation.networks import build_co_occurrence_graph
@@ -12,6 +14,11 @@ from tronn.interpretation.networks import sort_subgraphs_by_output_strength
 from tronn.interpretation.networks import build_subgraph_per_example_array
 from tronn.interpretation.networks import add_graphics_theme_to_nx_graph
 from tronn.interpretation.networks import annotate_subgraphs_with_pwm_scores
+from tronn.interpretation.networks import build_full_graph
+from tronn.interpretation.networks import get_maxsize_k_subgraphs
+from tronn.interpretation.networks import attach_mut_logits
+from tronn.interpretation.networks import write_bed_from_graph
+from tronn.interpretation.networks import stringize_nx_graph
 
 from tronn.util.utils import DataKeys
 
@@ -22,15 +29,66 @@ def run(args):
     # setup
     logger = logging.getLogger(__name__)
     logger.info("Building grammars")
+    
+    # get sig pwms vector
+    with h5py.File(args.sig_pwms_file, "r") as hf:
+        sig_pwms = hf[args.sig_pwms_key][args.foreground_targets]["sig"][:]
+        sig_pwms_indices = np.where(sig_pwms != 0)[0]
+    
+    # make graph
+    graph = build_full_graph(
+        args.scan_file,
+        sig_pwms_indices,
+        min_positive_tasks=args.min_positive_tasks,
+        min_region_num=args.min_support)
 
-    # adjust for sig file as needed
-    if args.sig_pwms_file is None:
-        args.sig_pwms_file = args.scan_file
+    # get subgraphs
+    subgraphs = get_maxsize_k_subgraphs(graph, args.min_support)
+
+    # attach delta logits to nodes
+    subgraphs = attach_mut_logits(subgraphs, args.scan_file)
+
+    # for each subgraph, produce:
+    # 1) gml file that keeps the network structure
+    # 3) BED file of regions to check functional enrichment
+    colors = sns.color_palette("hls", len(subgraphs)).as_hex()
+    for subgraph_idx in xrange(len(subgraphs)):
+        grammar_prefix = "grammar-{}".format(subgraph_idx)
+        grammar_file_prefix = "{}/{}.{}.{}".format(
+            args.out_dir, args.prefix, args.foreground_targets, grammar_prefix)
+        grammar_gml = "{}.gml".format(grammar_file_prefix)
+        grammar_bed = "{}.bed".format(grammar_file_prefix)
+
+        # get subgraph
+        subgraph = subgraphs[subgraph_idx]
+
+        # adjust edge color
+        for start_node, end_node, data in subgraph.edges(data=True):
+            data["graphics"] = {"fill": colors[subgraph_idx].upper()}
+
+        # write out
+        write_bed_from_graph(subgraph, grammar_bed, merge=True)
+        nx.write_gml(stringize_nx_graph(subgraph.copy()), (grammar_gml))
+
+    quit()
         
-    # extract metadata
-    with h5py.File(args.scan_file, "r") as hf:
-        metadata = hf[DataKeys.SEQ_METADATA][:]
-        
+    nodes_w_delta = []
+    for i in range(len(subgraphs)):
+        average_change = [data["deltalogits"][5] for node, data in subgraphs[i].nodes(data=True)]
+        average_change = np.max(np.array(average_change))
+        #print subgraphs[i].nodes, average_change
+        nodes_w_delta.append((list(subgraphs[i].nodes), average_change))
+
+    sorted_subgraphs = sorted(nodes_w_delta, key=lambda x:x[1])
+    for subgraph in sorted_subgraphs:
+        print subgraph
+    
+    import ipdb
+    ipdb.set_trace()
+    
+
+    quit()
+    
     # set up targets
     with h5py.File(args.sig_pwms_file, "r") as hf:
         target_keys = hf[args.sig_pwms_key].keys()
--- tronn/interpretation/networks.py
+++ tronn/interpretation/networks.py
@@ -6,6 +6,7 @@ import h5py
 import copy
 import glob
 import logging
+import itertools
 
 import numpy as np
 import pandas as pd
@@ -639,7 +640,329 @@ def build_dmim_graph(
     return graph
 
 
+def _build_nodes(
+        all_sig_mut_outputs,
+        example_metadata,
+        pwm_indices,
+        pwm_names,
+        sig_reduce_type="any"):
+    """build nodes. when building, build with following attributes:
+    1) examples
+    2) global_idx (based on pwmvector)
+    """
+    if sig_reduce_type == "all":
+        reduce_fn = np.all
+    elif sig_reduce_type == "any":
+        reduce_fn = np.any
+    else:
+        raise ValueError("reduce fn is not valid!")
+
+    nodes = []
+    for pwm_i in range(len(pwm_indices)):
+        pwm_global_idx = pwm_indices[pwm_i]
+        pwm_name = pwm_names[pwm_global_idx]
+        pwm_sig_mut_outputs = all_sig_mut_outputs[:,pwm_i] # {N, logit}
+        pwm_sig_mut_outputs_reduced = reduce_fn(pwm_sig_mut_outputs != 0, axis=1) # {N}
+        example_indices = np.where(pwm_sig_mut_outputs_reduced)[0]
+        node_examples = set(example_metadata[example_indices])
+        node_attrs = {
+            "examples": node_examples,
+            "numexamples": len(node_examples),
+            "mutidx": pwm_i,
+            "pwmidx": pwm_global_idx}
+        nodes.append((pwm_name, node_attrs))
+        logging.info("{} has {} sig mutant examples".format(pwm_name, len(node_examples)))
+    logging.info("created {} nodes".format(len(nodes)))
+        
+    return nodes
+
+
+def _build_co_occurrence_edges(
+        graph,
+        min_region_num,
+        sig_reduce_type="any"): # TODO - consider, would like co-occurrence to be at the same time?
+    """build co-occurrence edges
+    """
+    nodes = list(graph)
+    edges = []
+    for node1_idx in range(len(nodes)):
+        node1_name = nodes[node1_idx]
+        for node2_idx in range(len(nodes)):
+            node2_name = nodes[node2_idx]
+            if node1_idx >= node2_idx:
+                continue
+            
+            # check co-occurrence
+            node1_examples = graph.nodes[node1_name]["examples"]
+            node2_examples = graph.nodes[node2_name]["examples"]
+            intersect = node1_examples.intersection(node2_examples)
+
+            if len(intersect) > min_region_num:
+                # make an edge
+                edge_attrs = {
+                    "examples": intersect,
+                    "numexamples": len(intersect),
+                    "edgetype": "co_occurrence"}
+                edge = (node1_name, node2_name, edge_attrs)
+                edges.append(edge)
+
+    logging.info("created {} co-occurrence edges".format(len(edges)))
+
+    return edges
+
+
+def _build_synergy_edges(
+        graph,
+        dmim_scores,
+        example_metadata,
+        min_region_num,
+        sig_reduce_type="any"):
+    """get synergy edges
+    """
+    if sig_reduce_type == "all":
+        reduce_fn = np.all
+    elif sig_reduce_type == "any":
+        reduce_fn = np.any
+    else:
+        raise ValueError("reduce fn is not valid!")
     
+    nodes = list(graph)
+    edges = []
+    for driver_node_idx in range(len(nodes)):
+        driver_node_name = nodes[driver_node_idx]
+        for responder_node_idx in range(len(nodes)):
+            responder_node_name = nodes[responder_node_idx]
+
+            # for now, ignore self loops
+            if driver_node_idx == responder_node_idx:
+                continue
+            
+            # check synergy
+            driver_mut_idx = graph.nodes[driver_node_name]["mutidx"]
+            responder_global_idx = graph.nodes[responder_node_name]["pwmidx"]
+            dmim_subset = dmim_scores[:,driver_mut_idx,:,responder_global_idx]
+            dmim_results = reduce_fn(dmim_subset != 0, axis=1)
+            example_indices = np.where(dmim_results)[0]
+            examples = set(example_metadata[example_indices])
+
+            if len(examples) > min_region_num:
+                # make an edge
+                edge_attrs = {
+                    "examples": examples,
+                    "numexamples": len(examples),
+                    "edgetype": "directed"}
+                edge = (driver_node_name, responder_node_name, edge_attrs)
+                edges.append(edge)
+
+    logging.info("created {} synergy edges".format(len(edges)))
+    for edge in edges:
+        print edge[0], edge[1]
+        
+    return edges
+
+
+def build_full_graph(
+        dmim_h5_file,
+        sig_pwms_indices,
+        sig_mut_logits_key=DataKeys.MUT_MOTIF_LOGITS_SIG,
+        dmim_scores_key=DataKeys.DMIM_SCORES_SIG,
+        sig_responses=DataKeys.FEATURES,
+        min_positive_tasks=2,
+        min_region_num=1000):
+    """build full graph with co-occurrence and synergy edges
+    """
+    # instantiate empty graph
+    graph = nx.MultiDiGraph()
+
+    # 1) build nodes
+    # first extract sig mut logits to make nodes
+    with h5py.File(dmim_h5_file, "r") as hf:
+        example_metadata = hf[DataKeys.SEQ_METADATA][:,0]
+        sig_mut_outputs = hf[sig_mut_logits_key][:]
+        pwm_names = hf[DataKeys.FEATURES].attrs[AttrKeys.PWM_NAMES]
+    assert sig_mut_outputs.shape[1] == len(sig_pwms_indices)
+    
+    # TODO adjust logits as needed
+    #sig_mut_outputs = sig_mut_outputs[:,:,[0,1,2,3,4,5,6,9,10,12]]
+    
+    # build nodes
+    nodes = _build_nodes(
+        sig_mut_outputs,
+        example_metadata,
+        sig_pwms_indices,
+        pwm_names,
+        sig_reduce_type="any")
+    graph.add_nodes_from(nodes)
+    
+    # 2) build co-occurrence edges
+    co_occurrence_edges = _build_co_occurrence_edges(
+        graph, min_region_num, sig_reduce_type="any")
+    graph.add_edges_from(co_occurrence_edges)
+
+    # 3) build synergy edges
+    # TODO somewhere before this do a signifiance test
+    with h5py.File(dmim_h5_file, "r") as hf:
+        dmim_scores = hf[dmim_scores_key][:]
+    #assert sig_mut_outputs.shape[1] == len(sig_pwms_indices)
+    print dmim_scores.shape
+    synergy_edges = _build_synergy_edges(
+        graph,
+        dmim_scores,
+        example_metadata,
+        min_region_num,
+        sig_reduce_type="any")
+    graph.add_edges_from(synergy_edges)
+    
+    return graph
+
+
+def make_graph_examples_consistent(graph):
+    """make sure all the examples are consistent
+    """
+    node_names = list(graph)
+    start_node = node_names[0]
+    examples = graph.nodes[start_node]["examples"]
+
+    # intersect all nodes
+    for node_name, node_examples in graph.nodes(data="examples"):
+        examples = examples.intersection(node_examples)
+
+    # intersect all edges
+    for start_node_name, end_node_name, edge_examples in graph.edges(data="examples"):
+        examples = examples.intersection(edge_examples)
+    num_examples = len(examples)
+        
+    # replace
+    for node, data in graph.nodes(data=True):
+        data["examples"] = examples
+        data["numexamples"] = num_examples
+
+    for start_node, end_node, data in graph.edges(data=True):
+        data["examples"] = examples
+        data["numexamples"] = num_examples
+
+    graph.graph["examples"] = examples
+    graph.graph["numexamples"] = num_examples
+
+    return graph
+
+
+def remove_edge_and_check_consistent(graph, min_region_count):
+    """remove edge from graph, check if meets all criteria
+    """
+    graph_edges = list(graph.edges)
+    new_graphs = []
+    for start_node, end_node, _ in graph_edges:
+        new_graph = graph.copy()
+        new_graph.remove_edge(start_node, end_node)
+        if not nx.is_weakly_connected(new_graph):
+            continue
+        new_graph = make_graph_examples_consistent(new_graph)
+        if new_graph.graph["numexamples"] == graph.graph["numexamples"]:
+            continue
+        if new_graph.graph["numexamples"] > min_region_count:
+            new_graphs.append(new_graph)
+        new_graphs += remove_edge_and_check_consistent(new_graph, min_region_count)
+        
+    return new_graphs
+
+
+def get_size_k_subgraphs(graph, min_region_count, k=1):
+    """for size k, get all unique subgraphs
+    """
+    total = 0
+    subgraphs = []
+    current_subgraph_len = 0
+    for node_subset in itertools.combinations(graph, k):
+        subgraph = graph.subgraph(node_subset)
+
+        # check if all connected
+        if nx.is_weakly_connected(subgraph):
+
+            # check num examples
+            subgraph = make_graph_examples_consistent(subgraph.copy())
+            if subgraph.graph["numexamples"] > min_region_count:
+                subgraphs.append(subgraph)
+
+            # now recursively remove edges and check subgraphs for any new ones
+            other_subgraphs = remove_edge_and_check_consistent(
+                subgraph.copy(), min_region_count)
+            subgraphs += other_subgraphs
+
+        total += 1
+    
+    return subgraphs
+
+
+def get_maxsize_k_subgraphs(graph, min_region_count, k=3):
+    """get all subgraphs of size k
+    """
+    subgraphs = []
+    for i in range(k):
+        subgraphs += get_size_k_subgraphs(graph, min_region_count, i+1)
+        logging.info("Found {} subgraphs for size {}".format(len(subgraphs), i+1))
+        
+    return subgraphs
+
+
+def attach_mut_logits(subgraphs, dmim_h5_file):
+    """attach delta logits to subgraphs
+    """
+    # delta logits
+    with h5py.File(dmim_h5_file, "r") as hf:
+        logits = np.expand_dims(hf["logits.norm"][:], axis=1)
+        mut_logits = hf[DataKeys.MUT_MOTIF_LOGITS][:]
+        delta_logits = np.subtract(mut_logits, logits)
+        example_metadata = hf[DataKeys.SEQ_METADATA][:,0]
+
+    # for each subgraph, attach to nodes
+    for subgraph in subgraphs:
+        examples = sorted(list(subgraph.graph["examples"]))
+        example_indices = np.where(np.isin(example_metadata, examples))[0]
+        subgraph_delta_logits = delta_logits[example_indices]
+
+        for node, data in subgraph.nodes(data=True):
+            data["deltalogits"] = np.median(subgraph_delta_logits[:,data["mutidx"]], axis=0)
+
+    return subgraphs
+
+
+def write_bed_from_graph(graph, bed_file, interval_key="active", merge=True):
+    """take the examples and write out a BED file
+    """
+    grammar_examples = list(graph.graph["examples"])
+    with open(bed_file, "w") as fp:
+        for region_metadata in grammar_examples:
+            interval_types = region_metadata.split(";")
+            interval_types = dict([
+                interval_type.split("=")[0:2]
+                for interval_type in interval_types])
+            interval_string = interval_types[interval_key]
+
+            chrom = interval_string.split(":")[0]
+            start = interval_string.split(":")[1].split("-")[0]
+            stop = interval_string.split("-")[1]
+            fp.write("{}\t{}\t{}\n".format(chrom, start, stop))
+
+    if merge:
+        tmp_bed_file = "{}.tmp.bed".format(bed_file.split(".bed")[0])
+        os.system("mv {} {}".format(bed_file, tmp_bed_file))
+        os.system("cat {} | sort -k1,1 -k2,2n | bedtools merge -i stdin > {}".format(
+            tmp_bed_file, bed_file))
+        os.system("rm {}".format(tmp_bed_file))
+
+    return None
+
+
+
+
+
+
+
+
+
+
+
 def get_subgraphs(
         graph,
         subgraph,
@@ -904,7 +1227,7 @@ def stringize_nx_graph(nx_graph):
     # node attributes
     for node_name, node_attrs in nx_graph.nodes(data=True):
         for key in node_attrs.keys():
-            if isinstance(nx_graph.nodes[node_name][key], (list, set)):
+            if isinstance(nx_graph.nodes[node_name][key], (list, set, np.ndarray)):
                 nx_graph.nodes[node_name][key] = ",".join([
                     str(val) for val in nx_graph.nodes[node_name][key]])
         # adjust node name for nice output in cytoscape
