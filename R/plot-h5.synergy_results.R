8300fc747564cd20e359044529cab4b5fd54d302
vervacity <dskim89@gmail.com>
2019-02-07 16:58:25 -0800

    playing with synergy plotting

--- R/plot-h5.synergy_results.R
+++ R/plot-h5.synergy_results.R
@@ -20,8 +20,14 @@ colnames(data) <- labels
 
 
 
-data <- 2^data
-#data$diff <- data[,1] - data[,2]
+#data <- 2^data
+#data <- data[data[,1] > 0.1,]
+#data <- data[data[,2] > 0.1,]
+data$diff <- data[,1] - data[,2]
+data <- data[data$diff > -1,]
+data <- data[data$diff < 1,]
+
+print(head(data))
 
 # set up for ggplot
 data_melted <- melt(data)
@@ -30,8 +36,8 @@ colnames(data_melted)[1] <- "variable"
 print(head(data_melted))
 
 ggplot(data_melted, aes(x=variable, y=value, fill=variable)) +
-    geom_boxplot(alpha=0.6, position=position_dodge(width=0.9)) + # colour="black"
-    #geom_violin(alpha=0.6, position=position_dodge(width=0.9)) +
+    #geom_boxplot(alpha=0.6, position=position_dodge(width=0.9)) + # colour="black"
+    geom_violin(alpha=0.6, position=position_dodge(width=0.9)) +
     geom_point(
         alpha=0.5,
         position=position_jitterdodge(dodge.width=0.9, jitter.width=0.50)) + # size=3
@@ -40,7 +46,7 @@ ggplot(data_melted, aes(x=variable, y=value, fill=variable)) +
     theme(
         axis.title=element_text(size=32),
         axis.text.y=element_text(size=16),
-        axis.text.x=element_text(size=12, angle=45, hjust=1),
+        axis.text.x=element_text(size=12, angle=30, hjust=1),
         legend.title=element_text(size=32),
         legend.text=element_text(size=12)) +
                                         #scale_fill_hue(l=60, c=35) +
