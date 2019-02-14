2cd2e9ff65dec4931e64bd6f4cd97f8c08557228
vervacity <dskim89@gmail.com>
2019-02-14 15:09:25 -0800

    clean up mutate and tests, separate mut positions from blanking mask

--- tronn/nets/mutate_nets.py
+++ tronn/nets/mutate_nets.py
@@ -69,8 +69,6 @@ class Mutagenizer(object):
         """adjust positions for those with worst gradients (which are also the
         base pairs to change to)
         """
-        # positions {N, mutM}
-        # grad_mins {N, 1, 1000, 4}
         window_size = 10
         
         # set up mask positions
@@ -86,10 +84,8 @@ class Mutagenizer(object):
             mut_masks.append(mut_positions)
         mut_masks = tf.concat(mut_masks, axis=1) # {N, mutM, 1000, 1}
         
-        # multiply with grad mins
-        # TODO try using the HIGHEST importance position, and replace with WORST gradient base pair
+        # use HIGHEST importance position to mark best spot to mutate
         importance_max = tf.reduce_max(importances, axis=(1,3), keepdims=True) # {N, 1, 1000, 1}
-        #gradients = tf.reduce_min(gradients, axis=1, keepdims=True) # {N, 1, 1000, 4}
         masked_importance_max = tf.multiply(importance_max, mut_masks) # {N, mutM, 1000, 1}
         masked_importance_max = tf.reduce_max(masked_importance_max, axis=-1) # {N, mutM, 1000}
 
@@ -121,7 +117,23 @@ class Mutagenizer(object):
         inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX_MUT] = positions
         
         return inputs, params
+
     
+    @staticmethod
+    def setup_blanking_mask(positions, blanking_width=10):
+        """given positions tensor, set up a blanking mask
+        """
+        positions = tf.reduce_max(positions, axis=-1, keepdims=True) # {1, 1000, 1}
+        positions = tf.expand_dims(positions, axis=0) # {1, 1, 1000, 1}
+        positions = tf.nn.max_pool(
+            positions,
+            [1,1,blanking_width,1],
+            [1,1,1,1],
+            padding="SAME") # {1, 1, 1000, 1}
+        positions = tf.squeeze(positions, axis=0) # {1, 1000, 1}
+        
+        return positions
+
     
     @staticmethod
     def point_mutagenize_example(tensors):
@@ -129,9 +141,8 @@ class Mutagenizer(object):
         """
         # extract tensors
         example = tensors[0] # {1, 1000, 4}
-        motif_present = tensors[1]
-        positions = tensors[2] # {1, 1000, 1}
-        grad_mins = tensors[3] # {1, 1000, 4}
+        positions = tensors[1] # {1, 1000, 1}
+        grad_mins = tensors[2] # {1, 1000, 4}
 
         # adjust positions for tf.where
         positions = tf.concat([positions]*4, axis=2) # {1, 1000, 4}
@@ -141,11 +152,8 @@ class Mutagenizer(object):
             tf.equal(positions, 0),
             example,
             grad_mins) # {1, 1000, 4}
-
-        # but only actually adjust if the motif actually there
-        example = tf.cond(motif_present, lambda: example_mut, lambda: example)
         
-        return example, positions
+        return example_mut
 
 
     @staticmethod
@@ -154,16 +162,10 @@ class Mutagenizer(object):
         """
         # extract tensors
         example = tensors[0] # {1, 1000, 4}
-        motif_present = tensors[1]
-        positions = tensors[2] # {1, 1000, 1}
-        k = 10
+        positions = tensors[1] # {1, 1000, 1}
         
         # adjustments
         example = tf.squeeze(example, axis=0) # {1000, 4}
-        positions = tf.expand_dims(positions, axis=0) # {1, 1, 1000, 1}
-        
-        # use pooling to get a spread
-        positions = tf.nn.max_pool(positions, [1,1,k,1], [1,1,1,1], padding="SAME") # {1, 1, 1000, 1}
         positions = tf.squeeze(positions) # {1000}
         num_positions = positions.get_shape().as_list()[0]
 
@@ -189,18 +191,22 @@ class Mutagenizer(object):
 
         # gather from example
         example_mut = tf.gather(example, final_indices)
+        example_mut = tf.expand_dims(example_mut, axis=0)
         
-        # conditional on whether the motif actually there
-        example = tf.cond(motif_present, lambda: example_mut, lambda: example)
-        example = tf.expand_dims(example, axis=0)
-        
-        # adjust positions
-        no_positions = tf.zeros(positions.get_shape())
-        positions = tf.cond(motif_present, lambda: positions, lambda: no_positions)
-        positions = tf.expand_dims(positions, axis=1)
-        positions = tf.expand_dims(positions, axis=0) # {1, 1000, 1}
-        
-        return example, positions
+        return example_mut
+
+    
+    @staticmethod
+    def position_indices_to_mask(position_indices, orig_seq_len, aux_mask=None):
+        """convert position indices {N, k} to one hot
+        """
+        mask = tf.one_hot(position_indices, orig_seq_len) # {N, k, 1000}
+        mask = tf.reduce_max(mask, axis=1, keepdims=True) # {N, 1, 1000}
+        mask = tf.expand_dims(mask, axis=3) # {N, 1, 1000, 1}
+        if aux_mask is not None:
+            mask = tf.multiply(mask, aux_mask) # apply any other mask that you might want
+
+        return mask
 
     
     def mutagenize_multiple_motifs(self, inputs, params):
@@ -214,6 +220,10 @@ class Mutagenizer(object):
         orig_seq_len = features.get_shape().as_list()[2]
         position_indices = inputs[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX_MUT]
         motif_present = inputs[DataKeys.MUT_MOTIF_PRESENT] # {N, mut_M}
+
+        # params
+        shuffle_width = params.get("mutate_shuffle_width", 10)
+        blank_width = params.get("mutate_mask_width", 14)
         
         # choose what kind of mutagenesis
         if self.mutation_type == "point":
@@ -222,29 +232,54 @@ class Mutagenizer(object):
         else:
             mutagenize_fn = self.shuffle_mutagenize_example
             replace_seq = tf.identity(features)
-            
-        all_mut_sequences = []
-        all_mut_positions = []
+
+        # set up edge mask to mask positions on edges
         edge_mask = tf.cast(tf.greater(tf.range(orig_seq_len), 10), tf.float32) # {1000}
-        edge_mask = tf.reshape(edge_mask, (1, 1, orig_seq_len))
+        edge_mask = tf.reshape(edge_mask, (1, 1, orig_seq_len, 1))
+
+        # go through each mut motif
+        all_mut_sequences = []
+        all_mut_masks = []
+        all_blanking_masks = []
         for mut_i in range(position_indices.get_shape().as_list()[1]):
-            
-            mut_positions = position_indices[:,mut_i,:] # {N, k}
-            mut_positions = tf.one_hot(mut_positions, orig_seq_len) # {N, k, 1000}
-            mut_positions = tf.multiply(edge_mask, mut_positions) # mask out positions at edge (not real)
-            mut_positions = tf.reduce_max(mut_positions, axis=1) # {N, 1000}
-            mut_positions = tf.expand_dims(mut_positions, axis=2) # {N, 1000, 1}
-            mut_positions = tf.expand_dims(mut_positions, axis=1) # {N, 1, 1000, 1}
+            mut_position_indices = position_indices[:,mut_i,:] # {N, k}
             mut_motif_present = motif_present[:,mut_i] # {N}
-            mut_sequences, mut_positions = tf.map_fn(
-                mutagenize_fn,
-                [features, mut_motif_present, mut_positions, replace_seq],
-                dtype=(tf.float32, tf.float32)) # {N, 1, 1000, 4}
-            all_mut_sequences.append(mut_sequences)
-            all_mut_positions.append(mut_positions)
+
+            # convert position indices to a position mask and also get a null mask
+            mut_mask = Mutagenizer.position_indices_to_mask(
+                mut_position_indices, orig_seq_len, aux_mask=edge_mask)
+            no_mut_mask = tf.zeros(mut_mask.get_shape())
+
+            # and adjust position mask based on whether the motif was present or not
+            mut_mask = tf.where(mut_motif_present, mut_mask, no_mut_mask)
+
+            # create a blanking mask (must do here to know exactly where orig POINT position was)
+            blank_mask = tf.nn.max_pool(
+                mut_mask, [1,1,blank_width,1],[1,1,1,1], padding="SAME") # {N, 1, 1000, 1}
+
+            # finally adjust if shuffle for larger window
+            if self.mutation_type == "shuffle":
+                mut_mask = tf.nn.max_pool(
+                    mut_mask, [1,1,shuffle_width,1], [1,1,1,1], padding="SAME") # {N, 1, 1000, 1}
             
+            # mutagenize
+            mut_sequences = tf.map_fn(
+                mutagenize_fn,
+                [features, mut_mask, replace_seq],
+                dtype=tf.float32) # {N, 1, 1000, 4}
+
+            # but only keep the mutant sequence if actually mutated
+            mut_sequences = tf.where(mut_motif_present, mut_sequences, replace_seq)
+            
+            # save out: mut sequence, mut mask, blank mask
+            all_mut_sequences.append(mut_sequences)
+            all_mut_masks.append(mut_mask)
+            all_blanking_masks.append(blank_mask)
+        
+        # outputs: mutated sequences, position mask of mutated positions, and a larger blanking mask
         outputs[DataKeys.MUT_MOTIF_ORIG_SEQ] = tf.stack(all_mut_sequences, axis=1) # {N, mut_M, 1, 1000, 4}
-        outputs[DataKeys.MUT_MOTIF_POS] = tf.stack(all_mut_positions, axis=1) # {N, mutM, 1, 1000, 1}
+        outputs[DataKeys.MUT_MOTIF_POS] = tf.stack(all_mut_masks, axis=1) # {N, mutM, 1, 1000, 1}
+        outputs[DataKeys.MUT_MOTIF_MASK] = tf.stack(all_blanking_masks, axis=1) # {N, mutM, 1, 1000, 1}
         
         return outputs, params
 
--- tronn/nets/mutate_nets_test.py
+++ tronn/nets/mutate_nets_test.py
@@ -7,7 +7,6 @@ import tensorflow as tf
 from tronn.nets.mutate_nets import Mutagenizer
 from tronn.util.utils import DataKeys
 
-# TODO - lots of tests to make sure the mutations are made correctly
 
 @pytest.fixture(scope="class")
 def one_hot_sequence(request):
@@ -21,17 +20,16 @@ def one_hot_sequence(request):
     request.cls.one_hot_sequence = sequence
 
 
-    
 
 @pytest.mark.usefixtures("one_hot_sequence")
 class MutagenizerTests(tf.test.TestCase):
 
 
-    def test_preprocess_for_shuffle(self):
+    def test_preprocess_for_shuffle_mut(self):
         """make sure preprocess correctly pulls out values
         """
         # necessary inputs
-        indices = np.zeros((1,12,1))
+        indices = np.ones((1,12,1)) * np.reshape(500 + 20*np.arange(12), (1, 12, 1))
         vals = np.ones((1,12,1))
         sig_pwms = np.zeros((12))
         sig_pwms[[1,3,5]] = 1
@@ -52,8 +50,6 @@ class MutagenizerTests(tf.test.TestCase):
         # assert: create new val/idx tensors with correct shapes
         assert results[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_VAL_MUT].shape == (1,3,1)
         assert results[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX_MUT].shape == (1,3,1)
-
-        # TODO - check on the blanking mask produced
 
         
     def test_preprocess_for_point_mut(self):
@@ -113,31 +109,27 @@ class MutagenizerTests(tf.test.TestCase):
         assert np.all(np.equal(results[DataKeys.WEIGHTED_PWM_SCORES_POSITION_MAX_IDX_MUT], np.array(
             [515, 563, 600]).reshape((1,3,1))))
 
-        # TODO check on the blanking mask produced
-
 
     def test_shuffle_mutagenize_single(self):
-        """test shuffle mutagenizer
+        """test shuffle mutagenizer. assumes you have a spread of positions to shuffle
         """
         example = np.squeeze(self.one_hot_sequence, axis=1)
         motif_present = True
         positions = np.sum(np.zeros_like(example), axis=-1, keepdims=True)
-        positions[0,500,0] = 1
+        positions[0,495:505,0] = 1
 
         with self.test_session():
             # arrange
-            mutagenizer = Mutagenizer()
+            mutagenizer = Mutagenizer(mutation_type="shuffle")
             tensors = [
                 tf.convert_to_tensor(example, dtype=tf.float32),
-                tf.convert_to_tensor(motif_present),
                 tf.convert_to_tensor(positions, dtype=tf.float32)]
 
             # act: run preprocess fn and eval
             outputs = mutagenizer.shuffle_mutagenize_example(tensors)
             results = self.evaluate(outputs)
 
-        mut_example = results[0]
-        adjusted_positions = results[1]
+        mut_example = results
 
         # assert: only the shuffled part was changed
         left_unchanged = np.all(np.equal(example[:,:495], mut_example[:,:495]))
@@ -161,10 +153,9 @@ class MutagenizerTests(tf.test.TestCase):
         
         with self.test_session():
             # arrange
-            mutagenizer = Mutagenizer()
+            mutagenizer = Mutagenizer(mutation_type="point")
             tensors = [
                 tf.convert_to_tensor(example, dtype=tf.float32),
-                tf.convert_to_tensor(motif_present),
                 tf.convert_to_tensor(positions, dtype=tf.float32),
                 tf.convert_to_tensor(gradients, dtype=tf.float32)]
 
@@ -172,8 +163,7 @@ class MutagenizerTests(tf.test.TestCase):
             outputs = mutagenizer.point_mutagenize_example(tensors)
             results = self.evaluate(outputs)
 
-        mut_example = results[0]
-        adjusted_positions = results[1]
+        mut_example = results
 
         # assert: only the shuffled part was changed
         left_unchanged = np.all(np.equal(example[:,:500], mut_example[:,:500]))
@@ -183,7 +173,7 @@ class MutagenizerTests(tf.test.TestCase):
         shuffle_changed = np.any(np.equal(example[:,500], gradients[:,500]))
         assert shuffle_changed, "did not change sequence. is random, if fails try run tests again"
 
-
+        
     def test_mutagenize_multiple(self):
         """make sure properly mutagenizes multiple sites
         """
@@ -219,15 +209,21 @@ class MutagenizerTests(tf.test.TestCase):
         
         mut_example = results[DataKeys.MUT_MOTIF_ORIG_SEQ][0,0]
         left_unchanged = np.all(np.equal(example[:,:445], mut_example[:,:445]))
-        assert left_unchanged, "left is not unchanged"
+        assert left_unchanged, "left is not unchanged {}".format(
+            np.where(np.equal(example[:,:445], mut_example[:,:445]))[1])
         right_unchanged = np.all(np.equal(example[:,455:], mut_example[:,455:]))
         assert right_unchanged, "right is not unchanged"
         shuffle_changed = np.any(np.not_equal(example[:,445:455], mut_example[:,445:455]))
         assert shuffle_changed, "did not change sequence. is random, if fails try run tests again"
+
+        # TODO write a test here
+        print results[DataKeys.MUT_MOTIF_MASK][0,0,0,440:460,0]
+        print results[DataKeys.MUT_MOTIF_POS][0,0,0,440:460,0]
         
         mut_example = results[DataKeys.MUT_MOTIF_ORIG_SEQ][0,1]
         left_unchanged = np.all(np.equal(example[:,:470], mut_example[:,:470]))
-        assert left_unchanged, "left is not unchanged"
+        assert left_unchanged, "left is not unchanged {}".format(
+            np.where(np.equal(example[:,:470], mut_example[:,:470]))[0])
         right_unchanged = np.all(np.equal(example[:,480:], mut_example[:,480:]))
         assert right_unchanged, "right is not unchanged"
         shuffle_changed = np.any(np.not_equal(example[:,470:480], mut_example[:,470:480]))
