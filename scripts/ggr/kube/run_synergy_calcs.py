55b0d343b9842cf782d94276c3f7f24b4403bf2d
vervacity <dskim89@gmail.com>
2019-02-18 12:41:29 -0800

    convenience fns added

--- scripts/ggr/kube/run_synergy_calcs.py
+++ scripts/ggr/kube/run_synergy_calcs.py
@@ -14,7 +14,14 @@ def main():
 
     # get synergy files
     synergy_files = glob.glob("{}/*/ggr.synergy.h5".format(INFER_DIR))
-    
+
+    # other convenience set ups
+    plots_dir = "{}/plots".format(INFER_DIR)
+    os.system("mkdir -p {}".format(plots_dir))
+    grammars_dir = "{}/grammars.synergy_filt".format(INFER_DIR)
+    os.system("mkdir -p {}".format(grammars_dir))
+
+    # go through synergy files
     for synergy_file in synergy_files:
 
         # set up dir/prefix
@@ -29,6 +36,17 @@ def main():
         print calc_synergy
         #os.system(calc_synergy)
 
+        # cp plots out to separate folder for easy download
+        copy_plots = "cp {}/*pdf {}".format(out_dir, plots_dir)
+        os.system(copy_plots)
+        
+        # and copy grammars to separate folder for easy annotation
+        copy_grammar = "cp {}/*gml {}".format(out_dir, grammars_dir)
+        os.system(copy_grammar)
+        
+    # and annotate and collect
+    
+    
     return
 
 main()
