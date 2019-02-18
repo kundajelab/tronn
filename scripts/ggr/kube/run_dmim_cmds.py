a4058cd6603888676fb50c8d0840297969f979d0
vervacity <dskim89@gmail.com>
2019-02-18 11:47:05 -0800

    simplify nautilus runs

--- scripts/ggr/kube/run_dmim_cmds.py
+++ scripts/ggr/kube/run_dmim_cmds.py
@@ -3,38 +3,51 @@
 import os
 
 def main():
+    """run all dmim jobs 
     """
-    """
-
+    # setup
+    START_JOBS = False
+    job_file = "job.dmim.ggr.yaml"    
     jobs = {
         "early": [0, 7, 8, 9, 10, 11],
         "mid": [1, 12, 13, 14],
-        "late": [2, 3, 4, 5]
-    }
+        "late": [2, 3, 4, 5]}
 
-    job_file = "job.dmim.ggr.yaml"
-    
-    for job_set in sorted(jobs.keys()):
-        print job_set
+    # runs
+    if START_JOBS:
+        # start jobs
+        for job_set in sorted(jobs.keys()):
+            print job_set
 
-        for idx in jobs[job_set]:
-            adjust_job_set = "sed -i -e 's/early\|mid\|late/{}/g' {}".format(job_set, job_file)
-            print adjust_job_set
-            os.system(adjust_job_set)
-            adjust_job_name = "sed -i -e 's/traj-[0-9]\+/traj-{}/g' {}".format(idx, job_file)
-            print adjust_job_name
-            os.system(adjust_job_name)
-            adjust_job_foreground = "sed -i -e 's/LABELS=[0-9]\+/LABELS={}/g' {}".format(idx, job_file)
-            print adjust_job_foreground
-            os.system(adjust_job_foreground)
-            adjust_job_foreground_name = "sed -i -e 's/LABELS-[0-9]\+/LABELS-{}/g' {}".format(idx, job_file)
-            print adjust_job_foreground_name
-            os.system(adjust_job_foreground_name)
-            print ""
+            for idx in jobs[job_set]:
+                adjust_job_set = "sed -i -e 's/\"early\|\"mid\|\"late/\"{}/g' {}".format(job_set, job_file)
+                print adjust_job_set
+                os.system(adjust_job_set)
+                adjust_job_name = "sed -i -e 's/traj-[0-9]\+/traj-{}/g' {}".format(idx, job_file)
+                print adjust_job_name
+                os.system(adjust_job_name)
+                adjust_job_foreground = "sed -i -e 's/LABELS=[0-9]\+/LABELS={}/g' {}".format(idx, job_file)
+                print adjust_job_foreground
+                os.system(adjust_job_foreground)
+                adjust_job_foreground_name = "sed -i -e 's/LABELS-[0-9]\+/LABELS-{}/g' {}".format(idx, job_file)
+                print adjust_job_foreground_name
+                os.system(adjust_job_foreground_name)
 
-            import ipdb
-            ipdb.set_trace()
-    
+                run_job = "kubectl apply -f {}".format(job_file)
+                print run_job
+                os.system(run_job)
+
+    else:
+        # clean up (delete jobs)
+        for job_set in sorted(jobs.keys()):
+            print job_set
+            
+            for idx in jobs[job_set]:
+                job_name = "dk.job.ggr.dmim.basset.traj-{}".format(idx)
+                remove_job = "kubectl delete job {}".format(job_name)
+                print remove_job
+                os.system(remove_job)
+                
     return
 
 
