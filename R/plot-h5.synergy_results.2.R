49c9dc4d28af92113166e0e269158f3f107ecff2
vervacity <dskim89@gmail.com>
2019-02-18 21:33:04 -0800

    colour sample if given

--- R/plot-h5.synergy_results.2.R
+++ R/plot-h5.synergy_results.2.R
@@ -14,7 +14,8 @@ diff_sig_key <- args[4]
 dists_key <- args[5] # {N}
 max_dist_key <- args[6]
 out_prefix <- args[7]
-aux_keys <- args[8:length(args)]
+sample_key <- args[8]
+aux_keys <- args[9:length(args)]
 
 # read in data
 data <- h5read(h5_file, synergy_scores_key, read.attributes=TRUE)
@@ -24,6 +25,9 @@ dists <- h5read(h5_file, dists_key)
 max_dist <- as.numeric(h5read(h5_file, max_dist_key))
 labels <- attr(data, "labels")
 num_tasks <- dim(data)[1]
+if (nchar(sample_key) > 0) {
+    sample_bool <- h5read(h5_file, sample_key)
+}
 
 # run for every tasks
 for (task_idx in 1:length(num_tasks)) {
@@ -35,13 +39,17 @@ for (task_idx in 1:length(num_tasks)) {
     task_data$diffs <- aperm(diffs)[,task_idx]
     task_data$diff_sig <- aperm(diff_sig)[,task_idx]
     task_data$dists <- abs(dists)
+
+    if (nchar(sample_key) > 0) {
+        task_data$sample <- sample_bool
+    }
     
     # remove dists < 12 (overlap in pwms)
     task_data <- task_data[task_data$dists > 12,]
     
     # plot comparison between fold changes
     plot_file <- paste(out_prefix, ".fc_compare.pdf", sep="")
-    ggplot(task_data, aes(x=b, y=a)) +
+    p <- ggplot(task_data, aes(x=b, y=a)) +
         geom_point(data=subset(task_data, diff_sig==2), colour="black") +
         geom_point(data=subset(task_data, diff_sig==1), colour="gray") +
         labs(y=labels[1], x=labels[2]) +
@@ -53,11 +61,14 @@ for (task_idx in 1:length(num_tasks)) {
             legend.title=element_text(size=32),
             legend.text=element_text(size=12),
             legend.position="none")
+    if (nchar(sample_key) > 0) {
+        p + geom_point(data=subset(task_data, sample==2), colour="red")
+    }
     ggsave(plot_file, height=7, width=7)
     
     # plot with distance
     plot_file <- paste(out_prefix, ".dist_x_diff.pdf", sep="")
-    ggplot(task_data, aes(x=dists, y=diffs)) +
+    p + ggplot(task_data, aes(x=dists, y=diffs)) +
         geom_point(data=subset(task_data, diff_sig==2), colour="black") +
         geom_point(data=subset(task_data, diff_sig==1), colour="gray") +
         geom_vline(xintercept=max_dist, colour="gray", linetype="dashed") +
@@ -70,6 +81,9 @@ for (task_idx in 1:length(num_tasks)) {
             legend.title=element_text(size=32),
             legend.text=element_text(size=12),
             legend.position="none")
+    if (nchar(sample_key) > 0) {
+        p + geom_point(data=subset(task_data, sample==2), colour="red")
+    }
     ggsave(plot_file, height=7, width=7)
 
 }
