5b19819f7a900745cf4fb78f2380294ece367193
root <root@dk.pvc>
2019-02-21 23:23:14 +0000

    updates to synergy cmd to handle batch change on fly

--- scripts/ggr/kube/run_synergy_cmds.py
+++ scripts/ggr/kube/run_synergy_cmds.py
@@ -14,6 +14,7 @@ def main():
     INFER_DIR = sys.argv[3]
     OUT_DIR = sys.argv[4]
     grammars_file = "{}/grammars.annotated/grammar_summary.filt.dedup.txt".format(INFER_DIR)
+    bash_script = "/datasets/software/git/tronn/scripts/ggr/kube/synergy.ggr.bash"
     
     # now read in table
     grammars_df = pd.read_table(grammars_file)
@@ -44,9 +45,22 @@ def main():
             OUT_DIR,
             os.path.basename(grammar_file).split(".gml")[0])
         os.system("mkdir -p {}".format(out_dir))
-            
+
+        # adjust batch size in synergy run script based on num mut motifs
+        mut_nodes = grammars_df["nodes"].iloc[grammar_idx].split(",")
+        if len(mut_nodes) == 2:
+            batch_size = 20
+        elif len(mut_nodes) == 3:
+            batch_size = 10
+        else:
+            print "mistake!"
+        adjust_batch_size = "sed -i -e 's/batch_size [0-9]\+/batch_size {}/g' {}".format(batch_size, bash_script)
+        print adjust_batch_size
+        os.system(adjust_batch_size)
+        
         # go to synergy run script
-        run_cmd = "/datasets/software/git/tronn/scripts/ggr/kube/synergy.ggr.bash {} {} {} {} {} {}".format(
+        run_cmd = "{} {} {} {} {} {} {}".format(
+            bash_script,
             MODEL_DIR,
             MODEL,
             "{}/dmim.{}".format(INFER_DIR, traj_labels[0]),
