fa3dc2f0bf2e9dd42a3fb09e6342060b2d006f43
vervacity <dskim89@gmail.com>
2019-04-02 10:11:49 -0700

    reconcile gpu diff

--- scripts/ggr/kube/simulategrammars.ggr.bash
+++ scripts/ggr/kube/simulategrammars.ggr.bash
@@ -10,7 +10,7 @@ tronn simulategrammar \
       --fifo \
       --prediction_sample $4/ggr.dmim.prediction_sample.h5 \
       --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
-      --num_gpus 6 \
+      --num_gpus 4 \
       --inference_targets 0 1 2 3 4 5 6 9 10 12 \
       --prefix ggr \
       --pwm_file /datasets/ggr/1.0.0d/annotations/HOCOMOCOv11_core_pwms_HUMAN_mono.renamed.nonredundant.txt \
