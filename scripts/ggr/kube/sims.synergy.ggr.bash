e040226d91cfbd6364778ec7b8db7beef63acb22
vervacity <dskim89@gmail.com>
2019-06-14 12:47:23 -0700

    make new script for sims interaction scoring

--- scripts/ggr/kube/sims.synergy.ggr.bash
+++ scripts/ggr/kube/sims.synergy.ggr.bash
@@ -3,6 +3,11 @@ python setup.py develop;
 PREFIX="${1}/ggr.${2}.regr.pretrained.folds.testfold-"
 SUFFIX="/model.json"
 tronn synergy \
+      --data_format pwm_sims \
+      --background_regions /datasets/ggr/1.0.0d/annotations/ggr.atac.idr.negatives.bed.gz \
+      --fasta /datasets/annotations.hg19/hg19.genome.fa \
+      --num_samples 3 \
+      --fifo \
       --dataset_json $3/dataset.dmim.json $4 \
       --prediction_sample $3/ggr.dmim.prediction_sample.h5 \
       --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
