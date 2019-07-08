f38e3e575811e7f16db0acc59bd1e392c8f785a6
vervacity <dskim89@gmail.com>
2019-07-08 13:08:04 -0700

    build tracks initial code

---
+++ scripts/ggr/kube/buildtracks.ggr.bash
@@ -0,0 +1,16 @@
+cd /datasets/software/git/tronn;
+python setup.py develop;
+PREFIX="${1}/ggr.${2}.regr.pretrained.folds.testfold-"
+SUFFIX="/model.json"
+tronn buildtracks \
+      --bed_file $3 \
+      --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
+      --num_gpus 6 \
+      --inference_targets 0 1 2 3 4 5 6 9 10 12 \
+      --prefix ggr \
+      --fasta /datasets/annotations.hg19/hg19.genome.fa \
+      --batch_size 16 \
+      -o $4 \
+      >> $4/job.out \
+      2>> $4/job.err;
+
---
+++ tronn/buildtracks_cmd.py
@@ -0,0 +1,56 @@
+"""description: command for plotting regions
+"""
+
+import os
+import json
+import h5py
+import logging
+
+from tronn.preprocess.bed import bin_regions_sharded
+
+def run(args):
+    """command to plot a region
+    """
+    # set up
+    logger = logging.getLogger(__name__)
+    logger.info("Building bigwigs on regions")
+    if args.tmp_dir is not None:
+        os.system('mkdir -p {}'.format(args.tmp_dir))
+    else:
+        args.tmp_dir = args.out_dir
+
+    # TODO eventually put this in args
+    args.bin_width = 200
+    args.stride = 50
+    args.final_length = 1000
+        
+    # check BED file, and shard as needed
+    sharded_bed_file = "{}/inputs.sharded.bed.gz".format(args.tmp_dir)
+    bin_regions_sharded(
+        args.bed_file,
+        args.prefix,
+        args.bin_width,
+        args.stride,
+        args.final_length,
+        args.chromsizes)
+    args.bed_file = sharded_bed_file
+    
+    # collect a prediction sample for cross model quantile norm
+    if args.model["name"] == "ensemble":
+        true_sample_size = args.sample_size
+        args.sample_size = 1000
+        run_inference(args, warm_start=True)
+        args.sample_size = true_sample_size
+
+    # run inference
+    inference_files = run_inference(args)
+
+    # convert to bp resolution bed file
+
+    # sort
+
+    # get mean per base pair
+
+
+
+    return
