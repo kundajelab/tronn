2f25a14c3ebf636bb605571d1abfb51fde9085a6
vervacity <dskim89@gmail.com>
2019-07-08 14:01:34 -0700

    build tracks

--- scripts/ggr/kube/buildtracks.ggr.bash
+++ scripts/ggr/kube/buildtracks.ggr.bash
@@ -3,13 +3,15 @@ python setup.py develop;
 PREFIX="${1}/ggr.${2}.regr.pretrained.folds.testfold-"
 SUFFIX="/model.json"
 tronn buildtracks \
-      --bed_file $3 \
+      --data_format bed \
+      --data_files $3 \
+      --fasta /datasets/annotations.hg19/hg19.genome.fa \
+      --chromsizes /datasets/annotations.hg19/hg19.chrom.sizes \
+      --fifo \
       --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
       --num_gpus 6 \
       --inference_targets 0 1 2 3 4 5 6 9 10 12 \
       --prefix ggr \
-      --fasta /datasets/annotations.hg19/hg19.genome.fa \
-      --chromsizes /datasets/annotations.hg19/hg19.chrom.sizes \
       --batch_size 16 \
       -o $4 \
       >> $4/job.out \
--- tronn/buildtracks_cmd.py
+++ tronn/buildtracks_cmd.py
@@ -25,17 +25,19 @@ def run(args):
     args.bin_width = 200
     args.stride = 50
     args.final_length = 1000
-        
-    # check BED file, and shard as needed
-    bin_regions_sharded(
-        args.bed_file,
-        "{}/{}".format(args.tmp_dir, args.prefix),
-        args.bin_width,
-        args.stride,
-        args.final_length,
-        args.chromsizes)
-    args.data_files = sorted(glob.glob("{}/{}*filt.bed.gz".format(
-        args.tmp_dir, args.prefix)))
+    
+    # check BED files, and bin/shard as needed
+    assert args.data_format == "bed"
+    for data_file in args.data_files:
+        bin_regions_sharded(
+            data_file,
+            "{}/{}".format(args.tmp_dir, os.path.basename(data_file).split(".bed")[0]),
+            args.bin_width,
+            args.stride,
+            args.final_length,
+            args.chromsizes)
+    args.data_files = sorted(glob.glob("{}/*filt.bed.gz".format(
+        args.tmp_dir)))
     logging.info(";".join(args.data_files))
     
     # collect a prediction sample for cross model quantile norm
