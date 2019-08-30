742bc99d4aa4cbcf7df429d81da3d299a71b7045
vervacity <dskim89@gmail.com>
2019-08-30 12:08:21 -0700

    arg fix

--- scripts/ggr/kube/sims.predict.ggr.bash
+++ scripts/ggr/kube/sims.predict.ggr.bash
@@ -4,6 +4,7 @@ PREFIX="${1}/ggr.${2}.regr.pretrained.folds.testfold-"
 SUFFIX="/model.json"
 tronn predict \
       --data_format pwm_sims \
+      --dataset_pwm_file /datasets/ggr/1.0.0d/annotations/HOCOMOCOv11_core_pwms_HUMAN_mono.renamed.nonredundant.txt \
       --data_files $4 \
       --single_pwm \
       --count_range 1 6 \
@@ -12,7 +13,6 @@ tronn predict \
       --fifo \
       --prediction_sample $3/ggr.scanmotifs.prediction_sample.h5 \
       --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
-      --pwm_file /datasets/ggr/1.0.0d/annotations/HOCOMOCOv11_core_pwms_HUMAN_mono.renamed.nonredundant.txt \
       --num_gpus 6 \
       --prefix ggr \
       --batch_size 12 \
