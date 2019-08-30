4a5331f5fe7cb13f16e20fa4a59cbc82847b54a6
vervacity <dskim89@gmail.com>
2019-08-30 11:43:53 -0700

    updates

--- scripts/ggr/kube/run_simulatedensity_cmds.py
+++ scripts/ggr/kube/run_simulatedensity_cmds.py
@@ -17,6 +17,9 @@ def main():
     motifs_file = "{}/motifs.sig/motifs.adjust.diff.rna_filt.dmim/summary/ggr.pwms_patterns_summary.txt".format(
         INFER_DIR)
     bash_script = "/datasets/software/git/tronn/scripts/ggr/kube/sims.predict.ggr.bash"
+
+    # prediction sample
+    prediction_sample_dir = "/datasets/inference.2019-02-05/motifs.input_x_grad.background"
     
     # first get the desired motif list
     motifs = pd.read_csv(motifs_file, sep="\t", header=0, index_col=0)
@@ -25,70 +28,28 @@ def main():
     # go through motifs
     for motif in motifs:
 
-        # set up out dir
+        # set up out dir (but if it exists don't run)
         out_dir = "{}/{}".format(OUT_DIR, motif)
+        if os.path.isdir(out_dir):
+            continue
         os.system("mkdir -p {}".format(out_dir))
 
         # set up input file
-        motif_file = "{}/input.txt".format(out_dir)
+        input_file = "{}/input.txt".format(out_dir)
         pd.DataFrame(data=[motif]).to_csv(
             motif_file, header=False, index=False)
-
-        quit()
-        # if already exists don't rerun
         
         # set up run cmd
-        # TODO do I need the dataset json? maybe?
-        run_cmd = "{0} {1} {2} {3} {4}".format(
-            bash_script,
-            MODEL_DIR,
-            MODEL,
-            motifs_file,
-            out_dir)
-
-        
-        pass
-
-    quit()
-    
-    # first get the desired grammars table
-    
-    # now read in table
-    grammars_df = pd.read_table(grammars_file)
-    print grammars_df
-
-    # now set up run for each line
-    for grammar_idx in range(grammars_df.shape[0]):
-
-        # get grammar file
-        grammar_file = grammars_df["filename"].iloc[grammar_idx]
-        grammar_file = "{}/{}".format(INFER_DIR, grammar_file)
-        print grammar_file
-
-        # get traj label for dir with prediction sample
-        traj_label = os.path.basename(grammar_file).split(".")[1].split("_x")[0]
-        traj_dir = "{}/{}".format(INFER_DIR, traj_label)
-        
-        # adjust out dir
-        out_dir = "{}/{}".format(
-            OUT_DIR,
-            os.path.basename(grammar_file).split(".gml")[0])
-        os.system("mkdir -p {}".format(out_dir))
-
-        # if already exists don't rerun
-        if os.path.isfile("{}/ggr.simulategrammar.h5".format(out_dir)):
-            continue
-        
-        # go to synergy run script
         run_cmd = "{0} {1} {2} {3} {4} {5}".format(
             bash_script,
             MODEL_DIR,
             MODEL,
-            grammar_file,
-            traj_dir,
+            prediction_sample_dir,
+            input_file,
             out_dir)
         print run_cmd
-        os.system(run_cmd)
+
+        quit()
 
     return
 
--- scripts/ggr/kube/sims.predict.ggr.bash
+++ scripts/ggr/kube/sims.predict.ggr.bash
@@ -4,21 +4,20 @@ PREFIX="${1}/ggr.${2}.regr.pretrained.folds.testfold-"
 SUFFIX="/model.json"
 tronn predict \
       --data_format pwm_sims \
-      --data_files $5 \
+      --data_files $4 \
       --single_pwm \
       --count_range 1 6 \
       --background_regions /datasets/ggr/1.0.0d/annotations/ggr.atac.idr.negatives.bed.gz \
       --fasta /datasets/annotations.hg19/hg19.genome.fa \
       --fifo \
-      --dataset_json $3/dataset.dmim.json $4 \
-      --prediction_sample $3/ggr.dmim.prediction_sample.h5 \
+      --prediction_sample $3/ggr.scanmotifs.prediction_sample.h5 \
       --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
       --pwm_file /datasets/ggr/1.0.0d/annotations/HOCOMOCOv11_core_pwms_HUMAN_mono.renamed.nonredundant.txt \
       --num_gpus 6 \
       --prefix ggr \
       --batch_size 12 \
-      -o $7 \
-      >> $7/job.out \
-      2>> $7/job.err;
+      -o $5 \
+      >> $5/job.out \
+      2>> $5/job.err;
 
 
