2f9464bb9b05e5b965efe7fa17b23611765b8015
vervacity <dskim89@gmail.com>
2019-09-09 09:01:40 -0700

    convenience fn, metadata to bed

--- tronn/util/formats.py
+++ tronn/util/formats.py
@@ -1,7 +1,9 @@
 """description: tools for handling json formatting
 """
 
+import os
 import json
+import gzip
 
 import numpy as np
 
@@ -31,3 +33,36 @@ def write_to_json(data, out_file):
             sort_keys=True,
             indent=4,
             cls=NumpyEncoder)
+
+
+def array_to_bed(data, bed_file, interval_key="active", merge=True):
+    """take an array of metadata and extract out 
+    desired bed regions
+    """
+    assert bed_file.endswith(".gz")
+    
+    with gzip.open(bed_file, "w") as out:
+        for region_metadata in data:
+            interval_types = region_metadata.split(";")
+            interval_types = dict([
+                interval_type.split("=")[0:2]
+                for interval_type in interval_types])
+            interval_string = interval_types[interval_key]
+
+            chrom = interval_string.split(":")[0]
+            start = interval_string.split(":")[1].split("-")[0]
+            stop = interval_string.split("-")[1]
+            out.write("{}\t{}\t{}\n".format(chrom, start, stop))
+
+    if merge:
+        tmp_bed_file = "{}.tmp.bed.gz".format(bed_file.split(".bed")[0])
+        os.system("mv {} {}".format(bed_file, tmp_bed_file))
+        os.system((
+            "zcat {} | "
+            "sort -k1,1 -k2,2n | "
+            "bedtools merge -i stdin | "
+            "gzip -c > {}").format(
+            tmp_bed_file, bed_file))
+        os.system("rm {}".format(tmp_bed_file))
+
+    return None
