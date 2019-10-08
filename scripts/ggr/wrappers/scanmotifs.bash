806b691683331bc15da4a4bfed524868a8f3f8fb
vervacity <dskim89@gmail.com>
2019-10-08 14:56:09 -0700

    updates to scanmotifs scripts

--- scripts/ggr/wrappers/scanmotifs.bash
+++ scripts/ggr/wrappers/scanmotifs.bash
@@ -7,17 +7,17 @@ SUFFIX="/model.json"
 tronn scanmotifs \
       --data_dir /datasets/ggr/1.0.0d/h5 \
       --targets ATAC_SIGNALS.NORM H3K27ac_SIGNALS.NORM H3K4me1_SIGNALS.NORM \
-      --filter_targets $4 \
+      --filter_targets $3 \
       --fasta /datasets/annotations.hg19/hg19.genome.fa \
-      --batch_size 16 \
+      --batch_size $4 \
       --model ensemble quantile_norm models=${PREFIX}0${SUFFIX},${PREFIX}1${SUFFIX},${PREFIX}2${SUFFIX},${PREFIX}3${SUFFIX},${PREFIX}4${SUFFIX},${PREFIX}5${SUFFIX},${PREFIX}6${SUFFIX},${PREFIX}7${SUFFIX},${PREFIX}8${SUFFIX},${PREFIX}9${SUFFIX} \
-      --num_gpus $3 \
+      --num_gpus $5 \
       --inference_targets 0 1 2 3 4 5 6 9 10 12 \
       --use_filtering \
       --prefix ggr \
       --pwm_file /datasets/ggr/1.0.0d/annotations/HOCOMOCOv11_core_pwms_HUMAN_mono.renamed.nonredundant.txt \
       --add_rc_pwms \
-      -o $5 \
-      >> $5/job.out \
-      2>> $5/job.err;
+      -o $6 \
+      >> $6/job.out \
+      2>> $6/job.err;
 
