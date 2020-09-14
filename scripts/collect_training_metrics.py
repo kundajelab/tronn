7d407442531f5b4eaf9d3112a41eb595c87fa353
vervacity <dskim89@gmail.com>
2020-09-13 20:30:34 -0700

    script to collect class imbalances etc

---
+++ scripts/collect_training_metrics.py
@@ -0,0 +1,99 @@
+
+
+import os, sys
+import json
+import h5py
+
+import numpy as np
+import pandas as pd
+
+
+def aggregate_positives_negatives_per_task(
+        data_dir, 
+        h5_files,
+        label_keys=["ATAC_LABELS"]):
+    """aggregate
+    """
+    num_positives = None
+    total_examples = 0
+    for h5_i in range(len(h5_files)):
+        print h5_files[h5_i]
+        h5_file = "{}/{}".format(data_dir, h5_files[h5_i])
+
+        # get data
+        for label_key in label_keys:
+            with h5py.File(h5_file, "r") as hf:
+
+                # labels summed is positives
+                data = np.sum(hf[label_key][:], axis=0)
+
+                # total examples gives you negatives
+                total_examples += hf[label_key].shape[0]
+
+                # also get file names
+                data_files = hf[label_key].attrs["filenames"]
+                
+        # append on
+        if h5_i == 0:
+            # start fresh
+            num_positives = data
+        else:
+            # add on
+            num_positives += data
+        
+    # set up negatives
+    num_negatives = total_examples - num_positives
+
+    # make dataframe
+    results = pd.DataFrame({
+        "task": data_files,
+        "positives": num_positives,
+        "negatives": num_negatives})
+    
+    return results
+
+
+
+def main():
+    """get the training metrics for a dataset
+    """
+    data_dir = sys.argv[1]
+    model_dir = sys.argv[2]
+    model_prefix = sys.argv[3]
+    models = range(10)
+
+    splits = ["train", "validation", "test"]
+    label_keys = ["ATAC_LABELS"]
+
+    all_results = None
+    
+    # collect across models
+    for model_i in models:
+
+        for split in splits:
+        
+            # dataset json
+            dataset_json = "{}/{}{}/dataset.{}_split.json".format(
+                model_dir, model_prefix, model_i, split)
+
+            # load
+            with open(dataset_json, "r") as fp:
+                data_dict = json.load(fp)
+            data_files = data_dict["data_files"]
+            results = aggregate_positives_negatives_per_task(
+                data_dir, data_files, label_keys=label_keys)
+            results["split"] = split
+            results["model"] = "{}{}".format(model_prefix, model_i)
+
+            # merge
+            if all_results is None:
+                all_results = results
+            else:
+                all_results = pd.concat([all_results, results])
+                
+    # save out
+    all_results.to_csv("testing.txt", sep="\t", index=False)
+    
+    return
+
+main()
